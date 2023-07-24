8b4a9d7e31f01d547e2703bc0d05b09e19b0b969
choikiyeop <cky9709@naver.com>
2023-07-24 15:01:57 +0900

    recoil 환경 구성

---
+++ docs/recoil.md
@@ -0,0 +1,4 @@
+# Recoil
+> 상태관리 라이브러리
+## Getting Started
+[Recoil 시작하기](https://recoiljs.org/ko/docs/introduction/getting-started/)  
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RecoilRoot } from 'recoil'
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>,
+  <RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>,
+  </RecoilRoot>
 )
