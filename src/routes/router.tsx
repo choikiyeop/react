0139c24147fc6a48ff75c64b64a78de57bf01e23
choikiyeop <cky9709@naver.com>
2023-07-24 16:49:08 +0900

    에러 페이지 설정

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import ErrorPage from "../pages/ErrorPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>
+    element: <App />,
+    errorElement: <ErrorPage />
   }
 ])
