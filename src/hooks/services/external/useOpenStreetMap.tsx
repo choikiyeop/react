857c6873b2bc65cb9ed01dc5645bc9ff0cd9ab72
choikiyeop <cky9709@naver.com>
2023-11-17 10:05:47 +0900

    feat: openstreetmap api 구현

--- src/hooks/services/external/useOpenStreetMap.tsx
+++ src/hooks/services/external/useOpenStreetMap.tsx
@@ -1,13 +1,18 @@
-import { useQuery } from "@tanstack/react-query"
+import { keepPreviousData, useQuery } from "@tanstack/react-query"
 import axios from "axios"
 
-export const useSearchAddress = (addressKeyword: string | undefined) => {
+export const useSearchAddress = (addressKeyword: string | undefined, exclude_place_ids) => {
   return useQuery({
-    queryKey: ["searchAddress"],
+    queryKey: ["searchAddress", exclude_place_ids],
     queryFn: () =>
-      axios.get(`https://nominatim.openstreetmap.org/search?format=json&q=대한민국,${encodeURIComponent(addressKeyword)}`)
-        .then(res => res.data)
+      axios.get(`https://nominatim.openstreetmap.org/search?format=json&countrycodes=kr&q=${encodeURIComponent(addressKeyword)}&exclude_place_ids=${encodeURIComponent(exclude_place_ids.join(","))}`)
+        .then(res => {
+          console.log(res.data)
+          return res.data
+        })
         .catch(err => console.log(err)),
-    enabled: false,
+    enabled: exclude_place_ids.length !== 0,
+    refetchOnWindowFocus: false,
+    placeholderData: keepPreviousData
   })
 }
