a0984c5db77b433ec5e27c7277747d12276bb9d7
choikiyeop <cky9709@naver.com>
2023-12-04 16:12:49 +0900

    feat: 자바스크립트 페이지 groupBy 함수 추가

--- src/pages/laboratory/JavascriptPage.tsx
+++ src/pages/laboratory/JavascriptPage.tsx
@@ -2,10 +2,26 @@
 
 export default function JavascriptPage() {
 
+  const peoples = [
+    { name: "John", age: 2 },
+    { name: "Jane", age: 19 },
+    { name: "Jeff", age: 69 },
+  ]
+
+  const adultsOnly = ({ age }) => {
+    if (age < 7) return 'kid';
+    if (age < 21) return 'adolescent';
+    return 'adult';
+  }
+
+  const handlePeoples = () => {
+    const organized = Object.groupBy(peoples, adultsOnly)
+    console.log(organized)
+  }
 
   return (
     <div>
-      
+      <button onClick={handlePeoples}>조건에 따라 배열 그룹 짓기</button>
     </div>
   )
 }
