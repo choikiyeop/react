3ba1fc1b98fd43fe873049653b685b9e38571a9c
choikiyeop <cky9709@naver.com>
2023-12-04 11:56:43 +0900

    feat: dialog 태그를 사용한 모달 구현

--- src/pages/laboratory/DialogPage.tsx
+++ src/pages/laboratory/DialogPage.tsx
@@ -1,8 +1,9 @@
+import { useRef } from "react";
 import { dialogsState } from "@/store/store"
 import { useSetRecoilState } from "recoil"
 
-
 export default function DialogPage() {
+  const dialogRef = useRef<HTMLDialogElement>(null);
   const setDialogs = useSetRecoilState(dialogsState)
 
   const openDialog = () => {
@@ -15,8 +16,13 @@ export default function DialogPage() {
 
   return(
     <div>
+      <dialog ref={dialogRef}>
+        <p>dialog 태그를 사용한 Modal입니다.</p>
+        <button onClick={() => dialogRef.current?.close()}>닫기</button>
+      </dialog>
       <button onClick={openDialog}>Dialog 열기</button>
       <button onClick={openDialog2}>Dialog2 열기</button>
+      <button onClick={() => dialogRef.current?.showModal()}>dialog 태그 modal 열기</button>
     </div>
   )
 }
